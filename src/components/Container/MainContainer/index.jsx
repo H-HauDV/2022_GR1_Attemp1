20a3dce02c3356d0c060ad465f492e0ca19b8b72
H-HauDV <doanhau1407@gmail.com>
2022-06-10 13:18:51 +0700

    Init homepage

--- src/components/Container/MainContainer/index.jsx
+++ src/components/Container/MainContainer/index.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const MainContainer = () => {
   return (
-    <div>MainContainer</div>
+    <div className="gird gird-cols-1 md:gid-cols-2">
+      <div className="p-4 bg-blue-400 flex-1"></div>
+      <div></div>
+      <div></div>
+
+    </div>
   )
 }
 
