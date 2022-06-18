ac72d8c400a7bee70542e63865d1794f8a9ecd3e
H-HauDV <doanhau1407@gmail.com>
2022-06-18 08:30:01 +0700

    Homepage Update

--- src/components/Container/HomeContainer/index.jsx
+++ src/components/Container/HomeContainer/index.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 import Delivery from "../../../img/delivery.png";
 import HeroBG from "../../../img/heroBg.png";
+import I1 from "../../../img/i1.png";
 
 const HomeContainer = () => {
   return (
-    <section
-      className="grid grid-cols-1 md:grid-cols-2 gap-2 w-full"
-      id="home"
-    >
+    <section className="grid grid-cols-1 md:grid-cols-2 gap-2 w-full" id="home">
       <div
         className="py-2 flex-1 flex flex-col items-start
        justify-center gap-6"
@@ -47,9 +45,27 @@ const HomeContainer = () => {
           Order Now
         </button>
       </div>
-      <div className="py-2 flex-1 flex items-center">
-        <img src={HeroBG} className="ml-auto w-full lg:w-auto h-420 lg:h-650" alt="bg" />
-        <div className="w-full h-full absolute flex items-center justify-center"></div>
+      <div className="py-2 flex-1 flex items-center relative">
+        <img
+          src={HeroBG}
+          className="ml-auto w-full lg:w-auto h-420 lg:h-650"
+          alt="bg"
+        />
+        <div className="w-full h-full absolute top-0 left-0 flex items-center justify-center px-16 py-4">
+          <div
+            className="w-190 p-4 bg-cardOverlay backdrop-blur-md rounded-3xl
+           flex items-center justify-center flex-col"
+          >
+            <img src={I1} alt="I1" className="w-40 -mt-20" />
+            <p className="text-lg font-semibold text-textColor">IceCream</p>
+            <p className="text-sm text-lighttextGray font-semibold my-3">
+              Chocolate & Vanilla
+            </p>
+            <p className="text-sm font-semibold text-headingColor">
+              <span className="text-xs text-red-600">$</span> 5.25
+            </p>
+          </div>
+        </div>
       </div>
     </section>
   );
