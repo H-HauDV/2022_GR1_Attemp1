4b15de5f44e232788988e7c308f512bdeb18d072
H-HauDV <doanhau1407@gmail.com>
2022-06-24 23:15:38 +0700

    Update query get item firebase

--- src/utils/firebaseFunctions.js
+++ src/utils/firebaseFunctions.js
@@ -1,8 +1,17 @@
 import { firestore } from "../firebase.config";
-import { doc, setDoc } from "firebase/firestore";
+import { collection, doc, setDoc, getDocs, query, orderBy } from "firebase/firestore";
 //Saving new item
 export const saveItem = async (data) => {
   await setDoc(doc(firestore, "foodItems", `${Date.now()}`), data, {
     merge: true,
   });
+};
+
+// Get food items
+export const getAllFoodItems = async () => {
+  const items = await getDocs(
+    query(collection(firestore, "foodItems"), orderBy("id", "desc"))
+  );
+
+  return items.docs.map((doc) => doc.data());
 };
